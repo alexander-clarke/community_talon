ae3d18886344ff2d2221ce1e35a97af0ff73c699
Alex Clarke <ac3716@ic.ac.uk>
2024-06-29 15:00:11 +0100

    Add some checks to prevent focusing on invalid windows

--- core/app_switcher/app_switcher.py
+++ core/app_switcher/app_switcher.py
@@ -246,12 +246,16 @@ class Actions:
                 ):
                     name = full_application_name
                     break
-        for application in ui.apps(background=False):
-            if application.name == name or (
-                app.platform == "windows"
-                and application.exe.split(os.path.sep)[-1] == name
-            ):
-                return application
+        apps = ui.apps(background=False)
+        apps = [application for application in ui.apps(background=False) if application.name == name or (app.platform == "windows" and application.exe.split(os.path.sep)[-1] == name)]
+        print(apps)
+        if len(apps) > 1:
+          filtered_apps = list(filter(lambda application: is_valid_window(application.active_window), apps))
+          if filtered_apps:
+            apps = filtered_apps
+        print(list(apps))
+        if apps:
+          return apps[0]
         raise RuntimeError(f'App not running: "{name}"')
 
     def switcher_focus(name: str):
@@ -341,6 +345,16 @@ def update_launch_list():
 
     ctx.lists["self.launch"] = actions.user.create_spoken_forms_from_map(
         launch, words_to_exclude
+    )
+
+def is_valid_window(window: ui.Window) -> bool:
+    # return True
+    return (
+        True
+        #  and not window.hidden
+        and window.title != ""
+        and (window.rect.width > window.screen.dpi or window.rect.width != 0)
+        and (window.rect.height > window.screen.dpi or window.rect.height != 0)
     )
 
 
