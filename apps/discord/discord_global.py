d3e386d36f1cdf2cad6fd2e2c0fee112509a2eee
Alex Clarke <alexander-c@outlook.com>
2024-07-29 16:28:05 +0100

    make discord global moot also work for teams really should rename it I think

--- apps/discord/discord_global.py
+++ apps/discord/discord_global.py
@@ -1,26 +1,30 @@
-from talon import Module, app, actions
+from talon import Module, app, actions, ctrl, ui
 
 mod = Module()
-
 
 @mod.action_class
 class discord_global_actions:
   def discord_global_mute():
     """"""
-    if True:
-      actions.key("ctrl-alt-shift-t")
-    else:
-      focused_on_discord  = True
-      if not actions.app.name().lower() =="discord":
-        actions.user.switcher_focus("discord")
-        focused_on_discord = False
-      
-      actions.user.discord_mute()
-      
-      if not focused_on_discord:
-        actions.key("alt-tab")
+    
+    actions.key("ctrl-alt-shift-t")
+
+    #check for any teams running
+    if any(map(lambda app: app.name=="Microsoft Teams", ui.apps(background=False))):
+      # Unfortunately app=... doesn't work on windows we have to do the workaround
+      # ctrl.key_press("m", ctrl=True, shift=True, app="microsoft_teams")
+      current_app = actions.app.name()
+      focused_on_teams = current_app == "Microsoft Teams"
+      if not focused_on_teams:
+        actions.user.switcher_focus("Microsoft Teams")
+
+      #teams mute hotkey
+      actions.key("ctrl-shift-m")
+
+      if not focused_on_teams:
+        actions.user.switcher_focus(current_app)
+    
         
   def discord_global_screen_share():
     """"""
     actions.key("ctrl-alt-shift-s")
-      
