18b238551eae5c9d13a4271fa9cfe482e61ba3fe
Alex Clarke <alexander-c@outlook.com>
2024-11-25 10:36:50 +0000

    add option do not always refresh powershell title

--- apps/powershell/powershell_win.py
+++ apps/powershell/powershell_win.py
@@ -1,4 +1,4 @@
-from talon import Context, Module, actions, ui
+from talon import Context, Module, actions, ui, settings
 
 ctx = Context()
 mod = Module()
@@ -10,6 +10,13 @@ and win.title: /PowerShell/
 
 directories_to_remap = {}
 directories_to_exclude = {}
+
+mod.setting(
+    "powershell_always_refresh_title",
+    type=bool,
+    default=True,
+    desc="If the title is refreshed after every directory move",
+)
 
 
 @ctx.action_class("edit")
@@ -25,23 +32,12 @@ class UserActions:
             "$Host.UI.RawUI.WindowTitle = 'Windows PowerShell: ' +  $(get-location)"
         )
         actions.key("enter")
-        
-        '''
-in: ~\Documents\WindowsPowerShell
-function questue {
-    adb.exe shell "am broadcast -a android.intent.action.RUN -e cmd '$args'"
-}
-
-function prompt {
-  $Host.UI.RawUI.WindowTitle = 'Windows PowerShell: ' +  $(get-location)
-  "$pwd" + '> '
-}
-        '''
 
     def file_manager_open_parent():
         actions.insert("cd ..")
         actions.key("enter")
-        # actions.user.file_manager_refresh_title()
+        if settings.get("user.powershell_auto_refresh_title"):
+            actions.user.file_manager_refresh_title()
 
     def file_manager_current_path():
         path = ui.active_window().title
@@ -58,7 +54,8 @@ function prompt {
         """opens the directory that's already visible in the view"""
         actions.insert(f'cd "{path}"')
         actions.key("enter")
-        # actions.user.file_manager_refresh_title()
+        if settings.get("user.powershell_auto_refresh_title"):
+            actions.user.file_manager_refresh_title()
 
     def file_manager_select_directory(path: str):
         """selects the directory"""
@@ -70,8 +67,8 @@ function prompt {
 
     def file_manager_open_file(path: str):
         """opens the file"""
-        actions.insert(path)
-        # actions.key("enter")
+        actions.insert(f'./"{path}"')
+        actions.key("enter")
 
     def file_manager_select_file(path: str):
         """selects the file"""
