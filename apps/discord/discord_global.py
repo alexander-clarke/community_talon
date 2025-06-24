211c43e3e06da5a243bd6f3d34d707b156c0cdf7
Alex Clarke <alexander-c@outlook.com>
2025-06-24 14:41:54 +0100

    Make teams work better on windows

--- apps/discord/discord_global.py
+++ apps/discord/discord_global.py
@@ -2,29 +2,30 @@ from talon import Module, app, actions, ctrl, ui
 
 mod = Module()
 
+
 @mod.action_class
 class discord_global_actions:
-  def discord_global_mute():
-    """"""
-    
-    actions.key("ctrl-alt-shift-t")
+    def discord_global_mute():
+        """"""
+        actions.key("ctrl-alt-shift-t")
 
-    #check for any teams running
-    if any(map(lambda app: app.name=="Microsoft Teams", ui.apps(background=False))):
-      # Unfortunately app=... doesn't work on windows we have to do the workaround
-      # ctrl.key_press("m", ctrl=True, shift=True, app="microsoft_teams")
-      current_app = actions.app.name()
-      focused_on_teams = current_app == "Microsoft Teams"
-      if not focused_on_teams:
-        actions.user.switcher_focus("Microsoft Teams")
+        # check for any teams running
+        if any(
+            map(lambda app: app.name == "Microsoft Teams", ui.apps(background=False))
+        ):
+            # Unfortunately app=... doesn't work on windows we have to do the workaround
+            # ctrl.key_press("m", ctrl=True, shift=True, app="microsoft_teams")
+            current_app = actions.app.name()
+            focused_on_teams = current_app == "Microsoft Teams"
+            if not focused_on_teams:
+                actions.user.switcher_focus("Microsoft Teams")
 
-      #teams mute hotkey
-      actions.key("ctrl-shift-m")
+            # teams mute hotkey
+            actions.key("ctrl-shift-m")
 
-      if not focused_on_teams:
-        actions.user.switcher_focus(current_app)
-    
-        
-  def discord_global_screen_share():
-    """"""
-    actions.key("ctrl-alt-shift-s")
+            if not focused_on_teams:
+                actions.user.switcher_focus(current_app)
+
+    def discord_global_screen_share():
+        """"""
+        actions.key("ctrl-alt-shift-s")
--- apps/teams/teams.py
+++ apps/teams/teams.py
@@ -8,3 +8,10 @@ and app.name: /teams/
 os: linux
 and app.name: /Teams/
 """
+
+mod.apps.microsoft_teams = r"""
+os: windows
+and app.name: Microsoft Teams
+os: windows
+and app.exe: /^ms\-teams\.exe$/i
+"""
