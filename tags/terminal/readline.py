9700e35db2979eef77d1daa8c58a744602bf89a3
Alex Clarke <alexander-c@outlook.com>
2026-03-29 16:49:41 +0100

    Fixed duplication in readline

--- tags/terminal/readline.py
+++ tags/terminal/readline.py
@@ -90,66 +90,6 @@ compound_actions = {
 }
 
 
-# Wraps a method in a clip revert.
-# Might actually not be necessary for readline because in the default implementation it uses a seperate clipboard to the desktop
-# Though left in in case that has been modified
-def with_clip_revert(f: Callable) -> Callable:
-
-    def wrapped_method():
-        with clip.revert():
-            f()
-
-    return wrapped_method
-
-
-def with_selection_revert(f: Callable) -> Callable:
-    def wrapped_method():
-        f()
-        actions.key("ctrl-y")
-
-    return wrapped_method
-
-
-def cut_word():
-    actions.edit.word_left()
-    actions.user.cut_word_right()
-
-
-def cut_line_start():
-    actions.key("ctrl-x backspace")
-
-
-def cut_line_end():
-    actions.key("ctrl-k")
-
-
-compound_actions = {
-    # Delete
-    ("delete", "wordLeft"): with_clip_revert(actions.user.cut_word_left),
-    ("delete", "wordRight"): with_clip_revert(actions.user.cut_word_right),
-    ("delete", "word"): with_clip_revert(cut_word),
-    ("delete", "line"): with_clip_revert(actions.user.cut_line),
-    ("delete", "lineStart"): with_clip_revert(cut_line_start),
-    ("delete", "lineEnd"): with_clip_revert(cut_line_end),
-    # Copy
-    ("copyToClipboard", "wordLeft"): with_selection_revert(actions.user.cut_word_left),
-    ("copyToClipboard", "wordRight"): with_selection_revert(
-        actions.user.cut_word_right
-    ),
-    ("copyToClipboard", "word"): with_selection_revert(cut_word),
-    ("copyToClipboard", "line"): with_selection_revert(actions.user.cut_line),
-    ("copyToClipboard", "lineStart"): with_selection_revert(cut_line_start),
-    ("copyToClipboard", "lineEnd"): with_selection_revert(cut_line_end),
-    # Cut
-    ("cutToClipboard", "wordLeft"): actions.user.cut_word_left,
-    ("cutToClipboard", "wordRight"): actions.user.cut_word_right,
-    ("cutToClipboard", "word"): cut_word,
-    ("cutToClipboard", "line"): actions.user.cut_line,
-    ("cutToClipboard", "lineStart"): cut_line_start,
-    ("cutToClipboard", "lineEnd"): cut_line_end,
-}
-
-
 @ctx.action_class("user")
 class Actions:
     def cut_line():
